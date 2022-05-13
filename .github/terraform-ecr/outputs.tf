7416db62d7c3d0f162f0c657f70730aae891d5c4
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2022-05-13 20:01:15 +0200

    [fix] reusable terraform workflow

--- .github/terraform-ecr/outputs.tf
+++ .github/terraform-ecr/outputs.tf
@@ -1,9 +1,9 @@
 output "app_repository_name" {
-  value = aws_ecr_repository.ecr-app-repo.name
+  value     = aws_ecr_repository.ecr-app-repo.name
   sensitive = true
 }
 
 output "nginx_repository_name" {
-  value = aws_ecr_repository.ecr-nginx-repo.name
+  value     = aws_ecr_repository.ecr-nginx-repo.name
   sensitive = true
 }

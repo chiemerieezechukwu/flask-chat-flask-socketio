49d3a9bb73256f1ffd8281ef30c49527dc619959
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2022-07-18 18:53:10 +0200

    fix

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,9 +2,10 @@ data "aws_route53_zone" "hosted_zone" {
   name         = "chiemerie.com."
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "chatrrr_www" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = "chatrrr.chiemerie.com"
   type    = "A"
+  ttl = "300"
   records = [aws_lb.web-prod-lb.dns_name]
 }

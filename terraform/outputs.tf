b8884a3d5398422c1fe2b0b95702d6e6b5182ac1
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2023-01-05 23:24:44 +0100

    Done

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,13 @@
 output "alb_app_dns" {
   value = aws_lb.web-prod-lb.dns_name
 }
+
+output "app_repository_name" {
+  value     = aws_ecr_repository.ecr-app-repo.name
+  sensitive = true
+}
+
+output "nginx_repository_name" {
+  value     = aws_ecr_repository.ecr-nginx-repo.name
+  sensitive = true
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,13 +24,3 @@ variable "image_tag" {
   description = "Corresponds to a git tag."
   default     = "latest"
 }
-
-output "app_repository_name" {
-  value     = aws_ecr_repository.ecr-app-repo.name
-  sensitive = true
-}
-
-output "nginx_repository_name" {
-  value     = aws_ecr_repository.ecr-nginx-repo.name
-  sensitive = true
-}
